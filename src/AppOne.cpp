41dd1e9459069a23ada42f1982d7839756b9268c
Shawn Cicoria <github@cicoria.com>
2021-05-27 01:40:30 -0400

    comments removed

--- src/AppOne.cpp
+++ src/AppOne.cpp
@@ -3,47 +3,18 @@
 /******************************************************/
 
 #include "Particle.h"
-#line 1 "/Users/cicorias/g/learn/particle/appone/AppOne/src/AppOne.ino"
-// ------------
-// Blink an LED
-// ------------
-
-/*-------------
-
-We've heavily commented this code for you. If you're a pro, feel free to ignore it.
-
-Comments start with two slashes or are blocked off by a slash and a star.
-You can read them, but your device can't.
-It's like a secret message just for you.
-
-Every program based on Wiring (programming language used by Arduino, and Particle devices) has two essential parts:
-setup - runs once at the beginning of your program
-loop - runs continuously over and over
-
-You'll see how we use these in a second. 
-
-This program will blink an led on and off every second.
-It blinks the D7 LED on your Particle device. If you have an LED wired to D0, it will blink that LED as well.
-
--------------*/
-
-
-// First, we're going to make some variables.
-// This is our "shorthand" that we'll use throughout the program:
+#line 1 "/home/cicorias/g/learn/iot/particle/particle-argon-appone/src/AppOne.ino"
 
 void setup();
 void publish();
 void loop();
-#line 28 "/Users/cicorias/g/learn/particle/appone/AppOne/src/AppOne.ino"
+#line 2 "/home/cicorias/g/learn/iot/particle/particle-argon-appone/src/AppOne.ino"
 int led1 = D0; // Instead of writing D0 over and over again, we'll write led1
 // You'll need to wire an LED to this one to see it blink.
 
 int led2 = D7; // Instead of writing D7 over and over again, we'll write led2
 // This one is the little blue LED on your board. On the Photon it is next to D7, and on the Core it is next to the USB jack.
 
-// Having declared these variables, let's move on to the setup function.
-// The setup function is a standard part of any microcontroller program.
-// It runs only once when the device boots up or is reset.
 
 SYSTEM_THREAD(ENABLED);
 
